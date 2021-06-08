16ae4964e77468337f3ae64dc96a69b80c597810
HEMANG SINGH <hemang.cs19@bmsce.ac.in>
2021-06-08 19:37:15 +0530

    Add files via upload

--- Hemang/public/javascripts/Loginpage.js
+++ Hemang/public/javascripts/Loginpage.js
@@ -9,3 +9,69 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
+
+
+
+function validateUserEmail()
+{
+    var email = document.getElementById('Email').value;
+    var result = email.match('^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$');
+    if(result==email)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+function validateUserDetails()
+{
+    if(validateUserEmail() == true && document.getElementById('pwd').value != '')
+    {
+        location.replace("#");
+        return true;
+    }
+    else{
+        alert('Email Format is invalid or password field is empty');
+        location.replace("#")
+    }
+}
+function check_name() {
+    var x= document.getElementById("Name").value;
+    var nameError = document.getElementById('name-field1');
+    if(x==""||!(isNaN(x))) {
+        nameError.style.display = 'block';
+        return false;
+    }
+    else {
+        nameError.style.display = 'none';
+        return true;
+    }
+}
+function check_email() {
+    var email1 = document.getElementById('Email').value;
+    var result = email1.match('^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$');
+    var emailError = document.getElementById('email-field1');
+    if (result == email1) {
+        emailError.style.display = 'none';
+        return true;
+    }
+    else {
+        emailError.style.display = 'block';
+        return false;
+    }
+}
+function check_email1() {
+    var email1 = document.getElementById('Email1').value;
+    var result = email1.match('^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$');
+    var emailError = document.getElementById('email-field2');
+    if (result == email1) {
+        emailError.style.display = 'none';
+        return true;
+    }
+    else {
+        emailError.style.display = 'block';
+        return false;
+    }
+}
