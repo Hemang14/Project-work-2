101ae18948538ef065d8db16218babc4d7c1a580
HEMANG SINGH <hemang.cs19@bmsce.ac.in>
2021-07-03 10:26:53 +0530

    Add files via upload

--- Hemang/routes/index.js
+++ Hemang/routes/index.js
@@ -30,11 +30,25 @@ router.get('/register',function(req,res,next){
 });
 
 router.get('/users', function(req, res, next) {
-  if(req.session.login){
-    (req.session.user == "master")?res.render('user-master',{name :'master',film : 'joker'}):res.render('user-common',{name :req.session.user, film : 'joker'});
+      var sql1='SELECT * FROM MOVIES_SHOWS';
+      var sql2='SELECT * FROM feedback';
+      var sql3='SELECT * FROM users';
+      var sql4='SELECT * FROM user_review';
+       db.query(sql1, function (err, data1, fields) {
+         db.query(sql2, function (err, data2, fields) {
+           db.query(sql3, function (err, data3, fields) {
+             db.query(sql4, function (err, data4, fields) {
+
+           if (err) throw err;
+    if(req.session.login){
+    (req.session.user == "master")?res.render('user-master',{name :'master',film : 'joker',title:'User-List',movieData:data1,feedbackData:data1,userData:data3,reviewData:data4}):res.render('user-common',{name :req.session.user, film : 'joker'});
     return;
   }
   res.redirect('/login');
+            });
+          });
+        });
+      });
 });
 
 router.post('/register',(req,res,next)=>{
@@ -77,14 +91,28 @@ router.post('/review', function(req, res, next) {
   // store all the user input data
   const userDetails=req.body;
 
-  // insert user data into users table
-  var sql = 'INSERT INTO user_review SET ?';
-  db.query(sql, userDetails,function (err, data) {
+  // insert form data into user_review table
+  var sql1= 'INSERT INTO user_review SET ?';
+  var sql2='update movies_shows,user_review set movies_shows.rating = movies_shows.rating+user_review.star where movies_shows.movie_show_name = user_review.movie_name';
+  db.query(sql1, userDetails,function (err, data) {
+    db.query(sql2,function(err,result,fields){
       if (err) throw err;
-         console.log("User review dat is inserted successfully ");
+        console.log("User review dat is inserted successfully ");
+        console.log(result);
+      });
   });
  res.redirect('/users');  // redirect to create room page after inserting the data
 });
 // review form ends
 
+// for admin page start
+//
+// router.get('/users', function(req, res, next) {
+//     var sql='SELECT * FROM users';
+//     db.query(sql, function (err, data, fields) {
+//         if (err) throw err;
+//     res.render('user-master', { title: 'User List', userData: data});
+// });
+// });
+// admin page ends
 module.exports = router;
